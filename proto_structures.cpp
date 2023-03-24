de01fad1bc94be082809d2a075a47c5a25d65c5d
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2023-03-24 13:42:25 +0900

    Create proto_structures.cpp

---
+++ proto_structures.cpp
@@ -0,0 +1,29 @@
+#include "proto_structures.h"
+ipv4_addr::ipv4_addr(uint32_t x){
+	word=htonl(x);
+}
+ipv4_addr::ipv4_addr(const std::string& x){
+	std::istringstream ss(x);
+	char _;
+	for(int i=0;i<siz;i++){
+		if(i)ss>>_;
+		int tmp;ss>>tmp;
+		addr[i]=tmp;
+	}
+}
+ipv4_addr::ipv4_addr(const char *x){
+	for(int i=0;i<siz;i++){
+		int n;
+		sscanf(x,"%hhd%n",addr+i,&n);
+		x+=n+1;
+	}
+}
+ipv4_addr::operator std::string(){
+	std::ostringstream ss;
+	for(int i=0;i<siz;i++){
+		if(i)ss<<'.';
+		int tmp=addr[i];
+		ss<<tmp;
+	}
+	return ss.str();
+}
